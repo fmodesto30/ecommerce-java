8f8d35617f5027e82d7ae6eb9fe2fc6574ee6f07
Felipe Modesto <fmodesto30@gmail.com>
2023-07-12 14:56:10 -0300

    Adding Url solutions

--- domain/src/main/java/com/management/domain/Utils/AmazonReviewScraper.java
+++ domain/src/main/java/com/management/domain/Utils/AmazonReviewScraper.java
@@ -71,7 +71,10 @@ public class AmazonReviewScraper {
  		while (retryCount < maxRetries) {
 			
  			try {
- 			    setDocument(Jsoup.connect(url).get());
+ 				setDocument(Jsoup.connect(url).userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3")
+ 						.referrer("https://www.google.com")
+ 						.get());
+ 				
  			} catch (Exception e) {
  				throw new Exception("Product not found");
  			}
--- domain/src/main/resources/data.sql
+++ domain/src/main/resources/data.sql
@@ -1,12 +1,12 @@
 INSERT INTO 
 	TB_PRODUCT (ASIN, NAME, PRICE, DESCRIPTION, TIME) 
 VALUES
-  	('B09NRYKGB6', 'Fritadeira Elétrica sem oleo Electrolux Airfryer 4L 8 receitas pré-sugeridas desligamento automatico cesto removivel 1400W EAF30 grafite 127v por Rita Lobo', 'R$639,00', 'Kindle Paperwhite: agora com tela de 6,8” e bordas mais finas, temperatura de luz ajustável, bateria com duração de até 10 semanas e 20% mais rápido para virar as páginas', '2023-07-07 10:30:00' );
+  	('B098NY9KW8', 'Momcozy Muslin Swaddle Blankets', '$23,99', 'Material	Cotton Color	Brown Brand	Momcozy Special Feature	Skin Friendly, Lightweight, Durable Style	Modern', '2023-07-07 10:30:00' );
   	
   		
   	
 INSERT INTO 
 	TB_REVIEW (ASIN, FEEDBACK_CODE, CUSTOMER_NAME, FEEDBACK, FEEDBACK_DATE, RATING, TIME) 
 VALUES
-  	('B09NRYKGB6', 'R1ACMNW211KX54', 'Marcio','O produto veio sem o manual de instruções e sem o livro de receitas prometido no anúncio.', 'Avaliado no Brasil 🇧🇷 em 30 de abril de 2023', '1,0 de 5 estrelas', '2023-07-07 10:30:00' );
+  	('B098NY9KW8', 'R1ACMNW211KX54', 'nazanin',' I like the lightness and softness of the fabric. However, its not as soft as a proper muslin swaddle. The downside is that after 2 months of use,t it looks very used. It wont last for long.', 'Reviewed in the United States 🇺🇸 on April 12, 2023', '3.0 out of 5 stars', '2023-07-07 10:30:00' );
   	
